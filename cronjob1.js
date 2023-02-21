fcc1c6ed2bdba7f97a764599397d2340ccdfdcf4
Nguyễn Văn Phú <phunv@ftech.ai>
2023-02-21 10:13:49 +0700

    remove check bug

--- cronjob1.js
+++ cronjob1.js
@@ -64,34 +64,33 @@ module.exports = {
                         .catch(function (errorGet) {
                             console.log("[EXCEPTION] when call: " + DOMAIN + "count/1");
                         });
-                        console.log("num", num);
                         console.log("num", num, "count", result.data.count);
-                        // if(result != null && result.data.count <= 100)
-                        // {
-                        //     var index = arr.findIndex(x => x == num);
-                        //     let arrInsert = [];
-                        //     var symbol = num.toUpperCase();
-                        //     axios.get("https://api3.binance.com/api/v3/klines?symbol=" + symbol + "&interval=1h&limit=500").then(async (response) => {
-                        //         response.data.forEach((item) => {
-                        //             arrInsert.push({name: symbol, e: item[0], c: item[4], o: item[1], h: item[2], l: item[3], v: item[5], q: item[7], ut: updateTime, state: true});
-                        //         }); 
-                        //         if(arrInsert.length > 0)
-                        //         {
-                        //             var model = { num: index + 1, data:  arrInsert}
-                        //             var resInsert = await axios.post(DOMAIN + "syncDataClientVal", model)
-                        //             .catch(function (errorInsert) {
-                        //                 console.log("Exception when call: " + DOMAIN + "syncDataClientVal");
-                        //             });
-                        //             if(resInsert != null)
-                        //             {
-                        //                 console.log("resInsert:", num, resInsert.data, arrInsert.length);
-                        //             }
-                        //         }
-                        //     })
-                        //     .catch(function (error) {
-                        //         console.log("Exception when call: " + "https://api3.binance.com/api/v3/klines?symbol=" + symbol + "&interval=1h&limit=500");
-                        //     });
-                        // }
+                        if(result != null && result.data.count <= 100)
+                        {
+                            var index = arr.findIndex(x => x == num);
+                            let arrInsert = [];
+                            var symbol = num.toUpperCase();
+                            axios.get("https://api3.binance.com/api/v3/klines?symbol=" + symbol + "&interval=1h&limit=500").then(async (response) => {
+                                response.data.forEach((item) => {
+                                    arrInsert.push({name: symbol, e: item[0], c: item[4], o: item[1], h: item[2], l: item[3], v: item[5], q: item[7], ut: updateTime, state: true});
+                                }); 
+                                if(arrInsert.length > 0)
+                                {
+                                    var model = { num: index + 1, data:  arrInsert}
+                                    var resInsert = await axios.post(DOMAIN + "syncDataClientVal", model)
+                                    .catch(function (errorInsert) {
+                                        console.log("Exception when call: " + DOMAIN + "syncDataClientVal");
+                                    });
+                                    if(resInsert != null)
+                                    {
+                                        console.log("resInsert:", num, resInsert.data, arrInsert.length);
+                                    }
+                                }
+                            })
+                            .catch(function (error) {
+                                console.log("Exception when call: " + "https://api3.binance.com/api/v3/klines?symbol=" + symbol + "&interval=1h&limit=500");
+                            });
+                        }
                     }
                     catch(e)
                     {
